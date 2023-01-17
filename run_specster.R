cdcacce198bf512ee6b2f209f1ef61f83a637256
mattmeist <122904694+mattmeist@users.noreply.github.com>
2023-01-17 12:44:47 -0700

    Add files via upload

---
+++ run_specster.R
@@ -0,0 +1,130 @@
+pkgs <- c('foreach', 'doParallel', 'dplyr', 'dtplyr', 'lfe', 
+          'data.table')
+
+# Check for packages that are not installed
+new.pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+# Install the ones we need
+if(length(new.pkgs)) install.packages(new.pkgs)
+
+#Load them all in
+lapply(pkgs, library, character.only = TRUE)
+
+rm(pkgs, new.pkgs)
+
+# Note that the standard function uses 3 cores. You can ramp this up if needed.
+run_specifications <- function(data, models.df, model, cores = 3){
+  if (model != "lm" & model != "logistic" & model != "felm" & model != "lme") {
+    stop("Specify a model as either 'lm' (linear regression), 'logistic' (logistic regression), 
+         'felm' (fixed-effect linear regression), or 'lme' (mixed-effect linear regression)")
+  } else if (model == 'lm'){
+    #Create empty columns to hold results
+    models.df$B1.coef <- rep(NA, times = nrow(models.df))
+    models.df$B1.lb <- rep(NA, times = nrow(models.df))
+    models.df$B1.ub <- rep(NA, times = nrow(models.df))
+    models.df$B1.t <- rep(NA, times = nrow(models.df))
+    models.df$B1.p <- rep(NA, times = nrow(models.df))
+    models.df$R.Squared <- rep(NA, times = nrow(models.df))
+    models.df$Model <- 1:nrow(models.df)
+    # Run analysis
+    #get the core processors ready to go
+    registerDoParallel(cores=cores) 
+    # Loop
+    res <- foreach(i = 1:nrow(models.df), .combine='rbind') %dopar% {
+      mod <- summary(stats::lm(as.formula( #as.formula() allows us to paste things into a formula
+        paste(models.df$dv[i], "~",
+              models.df$x[i], " +", 
+              models.df$controls[i])), # Then how we cluster SEs 
+        data=data))
+      res <- c(mod$coefficients[1,1], #estimate
+               mod$coefficients[1,1] - 1.96 * mod$coefficients[1,2], #Lower-bound
+               mod$coefficients[1,1] + 1.96 * mod$coefficients[1,2], #upper-bound
+               mod$coefficients[1,3], #t val
+               mod$coefficients[1,4], # p val
+               mod$r.squared) # R2
+    }
+    # Now set the results
+    models.df$B1.coef <- res[,1]
+    models.df$B1.lb <- res[,2]
+    models.df$B1.ub <- res[,3]
+    models.df$B1.t <- res[,4]
+    models.df$B1.p <- res[,5]
+    models.df$R.Squared <- res[,6]
+    models.df
+  } else if (model == 'logistic'){
+    #Create empty columns to hold results
+    models.df$B1.coef <- rep(NA, times = nrow(models.df))
+    models.df$B1.lb <- rep(NA, times = nrow(models.df))
+    models.df$B1.ub <- rep(NA, times = nrow(models.df))
+    models.df$B1.z <- rep(NA, times = nrow(models.df))
+    models.df$B1.p <- rep(NA, times = nrow(models.df))
+    models.df$Model <- 1:nrow(models.df)
+    # Run analysis
+    #get the core processors ready to go
+    registerDoParallel(cores=cores) 
+    # Loop
+    res <- foreach(i = 1:nrow(models.df), .combine='rbind') %dopar% {
+      mod <- summary(stats::lm(as.formula( #as.formula() allows us to paste things into a formula
+        paste(models.df$dv[i], "~",
+              models.df$x[i], " +", 
+              models.df$controls[i],", family = 'binomial")),
+        data=data))
+      res <- c(mod$coefficients[1,1], #estimate
+               mod$coefficients[1,1] - 1.96 * mod$coefficients[1,2], #Lower-bound
+               mod$coefficients[1,1] + 1.96 * mod$coefficients[1,2], #upper-bound
+               mod$coefficients[1,3], #z val
+               mod$coefficients[1,4] # p val
+      )
+    }
+    # Now set the results
+    models.df$B1.coef <- res[,1]
+    models.df$B1.lb <- res[,2]
+    models.df$B1.ub <- res[,3]
+    models.df$B1.z <- res[,4]
+    models.df$B1.p <- res[,5]
+    models.df
+  } else if (model == 'felm'){
+    
+    #Create empty columns to hold results
+    models.df$B1.coef <- rep(NA, times = nrow(models.df))
+    models.df$B1.lb <- rep(NA, times = nrow(models.df))
+    models.df$B1.ub <- rep(NA, times = nrow(models.df))
+    models.df$B1.t <- rep(NA, times = nrow(models.df))
+    models.df$B1.p <- rep(NA, times = nrow(models.df))
+    models.df$R.Squared <- rep(NA, times = nrow(models.df))
+    models.df$Model <- 1:nrow(models.df)
+    # Run analysis
+    #get the core processors ready to go
+    registerDoParallel(cores=cores) 
+    # Loop
+    res <- foreach(i = 1:nrow(models.df), .combine='rbind') %dopar% {
+      mod <- summary(lfe::felm(as.formula( #as.formula() allows us to paste things into a formula
+        paste(models.df$dv[i], "~",
+              models.df$x[i], 
+              models.df$controls[i]," |", #Whatever controls are in row i, then the | to move to fixed effects
+              models.df$fe[i], "|",# Then the fixed effects
+              "0|", # Then the instrumental variables 
+              models.df$se[i])), # Then how we cluster SEs 
+        data=data))
+      res <- c(mod$coefficients[1,1], #estimate
+               mod$coefficients[1,1] - 1.96 * mod$coefficients[1,2], #Lower-bound
+               mod$coefficients[1,1] + 1.96 * mod$coefficients[1,2], #upper-bound
+               mod$coefficients[1,3], #t val
+               mod$coefficients[1,4], # p val
+               mod$r.squared) # R2
+    }
+    # Now set the results
+    models.df$B1.coef <- res[,1]
+    models.df$B1.lb <- res[,2]
+    models.df$B1.ub <- res[,3]
+    models.df$B1.t <- res[,4]
+    models.df$B1.p <- res[,5]
+    models.df$R.Squared <- res[,6]
+    models.df
+  }
+}
+
+
+
+
+
+
